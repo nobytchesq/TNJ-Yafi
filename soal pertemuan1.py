65f8e813d05b4dc816954ed05b8acd3d79df73ef
Muhammad El <152158730+nobytchesq@users.noreply.github.com>
2024-06-07 20:17:37 +0700

    Create soal pertemuan1.py

---
+++ soal pertemuan1.py
@@ -0,0 +1,19 @@
+print("Robot siap beroperasi")
+print("Robot bergerak maju")
+
+kecepatan = 10
+
+print("Kecepatan robot:", kecepatan)
+
+arah = "kiri"
+kecepatan = 5
+durasi = 10
+
+print("Robot bergerak ke", arah)
+print("Kecepatan:", kecepatan)
+print("Durasi:", durasi, "detik.")
+
+status1 = "Robot aktif"
+status2 = "Menunggu perintah"
+
+print("Status Robot", status1, "-", status2)
